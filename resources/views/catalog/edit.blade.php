1f1763ce389e591b5c8f78c35f8bed615b948eff
Hugo Suárez <qyb18074@educastur.es>
2025-03-10 13:24:48 +0100

    Macarrones

---
+++ resources/views/catalog/edit.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>VideoClub</title>
+</head>
+<body>
+@extends('layouts.master')
+@section('content')Modificar película {{$id}}
+@stop
+</body>
+</html>
